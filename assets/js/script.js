dfdeef57dadbeddefdeeb7e20deed0c887daecf6
Suri <swrshho@gmail.com>
2021-09-18 22:35:56 +0430

    changed browser's default date picker to flatpicker date picker and added search suggestions functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const searchInp = document.querySelector("#region");
+const suggestions = document.querySelector("#suggestions");
+const baseEndPoint = 'https://api.teleport.org/api/cities/?search=';
+
+searchInp.addEventListener('input', async () => {
+    let endpoint = baseEndPoint + searchInp.value;
+    let result = await (await fetch(endpoint)).json();
+    suggestions.innerHTML = '';
+    let cities = result._embedded['city:search-results'];
+    let length = cities.length > 5 ? 5 : cities.length;
+    for(let i = 0; i < length; i++) {
+        let option = document.createElement('option');
+        option.value = cities[i].matching_full_name;
+        suggestions.appendChild(option);
+    }
+})
