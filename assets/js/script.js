966595792fbd05d26a7c758d07aa19791eec3135
Suri <swrshho@gmail.com>
2021-09-28 22:18:37 +0330

    replaced flatpicker date picker with the HTML5 date picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ const suggestions = document.querySelector("#suggestions");
 const baseEndPoint = 'https://api.teleport.org/api/cities/?search=';
 
 searchInp.addEventListener('input', async () => {
-    let endpoint = baseEndPoint + searchInp.value;
-    let result = await (await fetch(endpoint)).json();
-    suggestions.innerHTML = '';
-    let cities = result._embedded['city:search-results'];
-    let length = cities.length > 5 ? 5 : cities.length;
-    for(let i = 0; i < length; i++) {
-        let option = document.createElement('option');
-        option.value = cities[i].matching_full_name;
-        suggestions.appendChild(option);
-    }
-})
+  let endpoint = baseEndPoint + searchInp.value;
+  let result = await (await fetch(endpoint)).json();
+  suggestions.innerHTML = '';
+  let cities = result._embedded['city:search-results'];
+  let length = cities.length > 5 ? 5 : cities.length;
+  for(let i = 0; i < length; i++) {
+    let option = document.createElement('option');
+    option.value = cities[i].matching_full_name;
+    suggestions.appendChild(option);
+  }
+});
